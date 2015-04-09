27897829e41b9dbd1bb811c3d19bf01c3e2ad10c
Mark Koudritsky <kamrik@gmail.com>
2015-04-08 20:10:00 -0400

    Add the full create task

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ if (args.android)
     platform = 'android'
 
 
-// Collect all the info for the cordova build of the app
+// Collect all the info for the cordova build of this app
 var prjInfo = {};
 prjInfo.platform = platform;
 prjInfo.paths = {};
@@ -54,10 +54,46 @@ gulp.task('clean', function(cb) {
 /******************************************************************************/
 
 // Create a platform project as a build artifact under ./build/<platform>/
+// This is the short version, see the full one below
 gulp.task('create', ['clean'], function() {
     fs.mkdirSync('build');
     var proj = new pp.PlatformProject();
     return proj.create(prjInfo);
+});
+
+/******************************************************************************/
+
+// Create a platform project as a build artifact under ./build/<platform>/
+// Full version. Only needed if you went to do some hackish things between the
+// stages. Can't handle icons well for now.
+gulp.task('longcreate', ['clean'], function() {
+    // TODO: only remove current platform build dir instead of full clean.
+    fs.mkdirSync('build');
+    var proj = new pp.PlatformProject();
+
+    // This is a list of PluginInfo objects
+    var plugins = proj.loadPlugins(path.resolve('./node_modules'));
+
+    //// Here is how to add a single plugin (with variables) from some other place
+    // var myPlugin = new pp.cdv.PluginInfo(path.resolve('./my_dev_plugin'))
+    // myPlugin.vars = {SOME_VAR: 'some_value'};
+    // plugins.push(myPlugin);
+
+    return proj.init(prjInfo)
+    .then(function(){
+        // addPlugins() can be called more than once.
+        return proj.addPlugins(plugins);
+    }).then(function(){
+        // This updates config files based on config.xml etc.
+        return proj.updateConfig(prjInfo.cfg);
+    }).then(function(){
+        // Copy www assets. This nneds to be called at least once to take care
+        // of cordova.js and some plugin assets.
+        return proj.copyWww(prjInfo.paths.www);
+    });
+
+    // At this stage you can run the platform build script as
+    // ./build/<platform>/cordova/build
 });
 /******************************************************************************/
 
@@ -67,10 +103,14 @@ gulp.task('build', [], function() {
     var proj = new pp.PlatformProject();
     return proj.open(platform, prjInfo.paths.root)
     .then(function() {
+        // Can be omitted or called after copyWww().
         return proj.updateConfig(prjInfo.cfg);
     })
     .then(function() {
         return proj.copyWww(prjInfo.paths.www);
+        // Alternatively you can copy the www files yourself to proj.wwwDir
+        // calling copyWww() at least once during create is still required
+        // to take care of cordova.js and plugin js scripts
     })
     .then(function() {
         return proj.build();
@@ -102,7 +142,6 @@ gulp.task('emulate', function() {
 
 // The default task for convenience
 gulp.task('default', function() {
-
     // I happen to have an Android device connected right now but not iOS.
     if (platform =='android')
         runSeq('build', 'run');
